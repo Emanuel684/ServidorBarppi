970cf04e1de6e5048bdbff57ad6bf67521710064
Emanuel684 <65985811+Emanuel684@users.noreply.github.com>
2020-11-30 00:47:20 -0500

    Lo que me recomendo albarez

--- index.js
+++ index.js
@@ -25,7 +25,27 @@ const { uuid } = require('uuidv4');
 const app = express();
 
 //CORS
-app.use(cors({origin: 'https://equipo8-sap-ppi-2020.vercel.app'}));
+app.use(cors({origin: '*'}));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://equipo8-sap-ppi-2020.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 // Middlewares
 app.use(morgan('dev'));
